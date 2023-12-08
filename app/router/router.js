9f1b86317c226762746b24c892b5f7cb45144adf
baharSaeedi <b.saeedi.ha@gmail.com>
2023-12-08 23:57:45 +0330

    create home controller of clientApi

--- app/router/router.js
+++ app/router/router.js
@@ -1,7 +1,9 @@
 const {Router} = require("express")
+const { homeRoutes } = require("./clientApi/home.router")
 
 const router = Router()
 
+router.use("/",homeRoutes)
 
 module.exports = {
     allRoutes : router
