1a27cc1e777a7f193be09667f9f3555d51c5f368
baharSaeedi <b.saeedi.ha@gmail.com>
2023-12-08 23:44:05 +0330

    base server congig

---
+++ app/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const http = require("http");
+const path = require("path");
+const { allRoutes } = require("./router/router");
+// const { appRoutes } = require("./router/router");
+
+module.exports = class Application {
+  #app = express();
+  #PORT
+  #DB_URL
+  constructor(PORT, DB_URL) {
+    this.#DB_URL=DB_URL;
+    this.#PORT=PORT;
+    this.configDatabase();
+    this.configApplication();
+    this.createServer();
+    this.createRoutes();
+    this.errorHandler();
+  }
+  configDatabase() {
+    mongoose.connect(this.#DB_URL).then(() => {
+      console.log("database connected");
+    });
+  }
+  configApplication() {
+    this.#app.use(express.json());
+    this.#app.use(express.urlencoded({ extended: true }));
+    this.#app.use(express.static(path.join(__dirname, "..", "public")));
+  }
+  createServer() {
+    const server = http.createServer(this.#app);
+    server.listen(this.#PORT, () => {
+      console.log(`app listen on http://localhost:${this.#PORT}`);
+    });
+  }
+  errorHandler() {
+    this.#app.use((req, res, nex) => {
+      return res.status(404).json({ success: false, message: "not found!" });
+    });
+    this.#app.use((error, req, res, next) => {
+      const status = error?.status || 500;
+      const message = error?.message || "internal server error";
+      return res.status(status).json({ success: false, message: message });
+    });
+  }
+  createRoutes() {
+    // this.#app.get("/", (req, res, next) => {
+    //   return res.status(200).json({
+    //     message: "hi",
+    //   });
+    // });
+    this.#app.use(allRoutes );
+  }
+};
---
+++ index.js
@@ -0,0 +1,2 @@
+const app = require("./app/app")
+new app (3000,"mongodb://localhost:27017/shopDB")
