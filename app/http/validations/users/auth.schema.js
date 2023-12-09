e2d95e362c8df94df71d96eec840bd43aa1f4faf
baharSaeedi <b.saeedi.ha@gmail.com>
2023-12-09 23:28:25 +0330

    fix bug in  auth schema

--- app/http/validations/users/auth.schema.js
+++ app/http/validations/users/auth.schema.js
@@ -1,7 +1,7 @@
 const joi = require("@hapi/joi")
 const authSchema = joi.object({
-    email : joi.lowercase().trim().string().email().required(),
-    password : joi.min(6).max(15).trim().string().required(),
+    email : joi.string().lowercase().trim().email().required(),
+    password : joi.string().min(6).max(15).trim().string().required(),
 })
 
 module.exports={
